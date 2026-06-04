6c6e146f0f8f27599440f0a72ea693c416931bbf
Daniel <daniel@andlaw.vn>
2026-06-04 08:39:18 +0700

    fix: configure SVGR webpack - SVG icons were returning metadata objects instead of React components (React error #130)

--- next.config.js
+++ next.config.js
@@ -9,6 +9,30 @@ const nextConfig = {
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
     ],
   },
+  webpack(config) {
+    // Configure SVGR: SVG files imported as React components
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    )
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i
+    }
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false,
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+            titleProp: true,
+          },
+        },
+      ],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
